fd82ceac2ae49f16af0f29f43eac3a7eac3e39dd
Bryce? <bryce.jones2026@gbwl.org>
2024-10-15 12:54:08 +0000

    started notes

--- Main.java
+++ Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      //One = sign ASSIGNS a value to a variable
+      int age = 15;
+      int year = 14;
+      // Two == is checking for EQUALITY
+      boolean match = (age == year);
+      System.out.println("Does age match year?" + match);
+      year = 15; //Re-ASSIGNING
+      System.out.println(age == year); // true
+
+      // != means "is Not equal to"
+      System.out.println(age != year); // false
+
+
+      // REALTIONAL OPERATORS
+      // used in boollean expression
+      double pi = 3.1415;
+      System.out.println(pi > 0);// checking if positive
+      System.out.println(pi < 0);// checking if negative
+      System.out.println(pi >= 3.14); // true
+      System.out.println(pi <= 10); //true
+      
+
    }
 }
