2e6c846cf1f41dd1869acac621e7cc61caf2555d
Bryce? <bryce.jones2026@gbwl.org>
2024-10-24 12:55:35 +0000

    ADDED NOTES

--- Notes.java
+++ Notes.java
@@ -1,11 +1,11 @@
 public class Notes {
 
-    public static void notes(String [] {
+    public static void main(String []args) {
         
 
         //MULTI-WAY SELECTION
         System.out.println("What type of costume are you thinking of? (scary, cute, funny, other)");
-        String choice = "scary";
+        String choice = "no";
         //Always start a "decision" with an IF statement
 
         if (choice.equals("scary")) {
@@ -24,6 +24,44 @@ public class Notes {
         else {
             System.out.println("Stick to the script or you're FIRED");
         }
+
+
+
+
+
+        // AND && OPERATOR
+        // Both conditions need to be true for the conditional body to be executed
+        boolean didHomework = false;
+        boolean cleanedRoom = true;
+        // can write the conditions either way below,
+        // both are valid because we're using boolean variables already
+        if ((didHomework == true) && (cleanedRoom)) {
+            System.out.println("GET OUT");
+
+        }
+
+        else {
+            System.out.println("GET BACK TO WORK");
+        }
+        // OR || OPERATOR
+        //Evaluates to TRUE if AT LEAST one expressuon is true
+        if ( (didHomework) || (cleanedRoom) ) {
+            System.out.println("Do it later");
+        }
+        else {
+            System.out.println("Do at least one thing bruh >:|");
+        }
+
+
+        // NOT ! OPERATOR
+        // Use on only one condition to flip the boolean result
+
+        boolean walkSignOn = true;
+        // Condition body will execute only if condition is FALSE
+        if (!walkSignOn) {
+            System.out.println("STOP CROSSING THE STREET!!!");
+        }
+
 
     }
 }
